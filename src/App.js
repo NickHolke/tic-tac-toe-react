7c82e1a6c9f710954dddbb43a0153499bd2491f8
Nick Holke <nickholke@gmail.com>
2020-02-03 20:36:02 -0800

    Styled basic board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Board = styled.div`
+  display: flex;
+  width: 600px;
+  height: 600px;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  border: 1px solid black;
+  margin: auto;
+`
+const Square = styled.div`
+  width: 200px;
+  height: 200px;
+  box-sizing: border-box;
+  border: 1px solid black;
+`
 
 function App() {
   return (
-    <div className="App">
-      Hello there
-    </div>
+    <Board>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </Board>
   );
 }
 
