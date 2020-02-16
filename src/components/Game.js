26d6c41ef014bee9ff0c6ca4d5567ae4bc5b1792
Nick Holke <nickholke@gmail.com>
2020-02-16 10:02:52 -0800

    Added computer turn;

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,7 +17,7 @@ export default class Game extends React.Component {
 
     this.state = {
       history: [{board: new Array(9).fill('')}],
-      turn: 'X',
+      turn: 'AI',
       round: 0,
       gameOver: false,
     }
@@ -27,17 +27,29 @@ export default class Game extends React.Component {
     this.restartGame = this.restartGame.bind(this);
   }
 
-  clickHandler(i, board) {
-    if (board[i] === '' && this.state.history.length === this.state.round + 1 && !this.state.gameOver) {
+  componentDidMount(){
+    const board = this.state.history[0].board;
+    this.computerTurn(board);
+  }
+
+  computerTurn(board) {
+    if (this.state.history.length === this.state.round + 1 && !this.state.gameOver) {
       let newBoard = board.slice();
-      newBoard[i] = this.state.turn;
-      
+      console.log(newBoard)
+
+      for (let i = 0; i < board.length; i++) {
+        if (newBoard[i] === '') {
+          newBoard[i] = 'X';
+          break;
+        }
+      }
+
       let history = this.state.history;
       history.push({board: newBoard});
 
       this.setState({
         history,
-        turn: this.state.turn === 'X' ? 'O' : 'X',
+        turn: 'Human',
         round: this.state.round + 1,
       });
 
@@ -46,6 +58,30 @@ export default class Game extends React.Component {
           gameOver: true,
         })
       }
+    }
+  }
+
+  clickHandler(i, board) {
+    if (board[i] === '' && this.state.history.length === this.state.round + 1 && !this.state.gameOver) {
+      let newBoard = board.slice();
+      newBoard[i] = 'O';
+      
+      let history = this.state.history;
+      history.push({board: newBoard});
+
+      if (isWon(newBoard)) {
+        this.setState({
+          gameOver: true,
+        })
+      }
+
+      this.setState({
+        history,
+        turn: 'AI',
+        round: this.state.round + 1,
+      }, () => {
+        this.computerTurn(newBoard);
+      }); 
     }
   }
 
@@ -58,7 +94,7 @@ export default class Game extends React.Component {
   restartGame() {
     this.setState({
       history: [{board: new Array(9).fill('')}],
-      turn: 'X',
+      turn: 'AI',
       round: 0,
       gameOver: false,
     });
@@ -67,13 +103,14 @@ export default class Game extends React.Component {
   render() {
     const round = this.state.round;
     const board = this.state.history[round].board;
+  
     return(
       <Wrapper>
         <Board board={board}
         clickHandler={this.clickHandler} />
-        <MoveHistory history={this.state.history}
+        {/* <MoveHistory history={this.state.history}
         turn={this.state.turn} jumpHandler={this.jumpHandler}
-        gameOver={this.state.gameOver} restartGame={this.restartGame}/>
+        gameOver={this.state.gameOver} restartGame={this.restartGame}/> */}
       </Wrapper>
     )
   }
